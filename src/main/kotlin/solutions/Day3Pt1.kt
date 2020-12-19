e0cdca1962aff61b4e7b7ef3a1056dcd1d0aff6f
tobie <tobiewee@gmail.com>
2020-12-20 00:44:57 +0800

    Added D3P1-TODO

---
+++ src/main/kotlin/solutions/Day3Pt1.kt
@@ -0,0 +1,24 @@
+package solutions
+
+import models.Input
+import solutions.`interface`.Puzzle
+
+class Day3Pt1(
+    private val fileName: String = "d3_input.txt"
+) : Puzzle(fileName) {
+
+    private fun countTreesOnR3D1Slope(input: Input): Int {
+        //TODO: Implement count for slope, moving right 3, and down 1
+        return 0
+    }
+
+    override fun run() {
+        val input = loadInput()
+        println("Day 2 (pt.2): ${countTreesOnR3D1Slope(input)}")
+    }
+
+    companion object {
+        private const val OPEN_SQUARE = "."
+        private const val TREE = "#"
+    }
+}
