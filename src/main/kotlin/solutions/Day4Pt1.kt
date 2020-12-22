09d0508a6ebf9990bc3077f1e4627f5a347efe9c
tobie <tobiewee@gmail.com>
2020-12-22 16:48:31 +0800

    Solved D4P1

--- src/main/kotlin/solutions/Day4Pt1.kt
+++ src/main/kotlin/solutions/Day4Pt1.kt
@@ -8,7 +8,51 @@ class Day4Pt1(
 ) : Puzzle(fileName) {
 
     private fun countValidPassports(input: Input): Int {
-        TODO("To be implemented")
+        var numValidPassports = 0
+        var passportValidator = PassportValidation()
+        input.inputData.indices.forEach {
+            val line = input.inputData[it]
+            when {
+                line.trim().isEmpty() -> {
+                    if (passportValidator.isValid()) {
+                        numValidPassports++
+                    }
+                    passportValidator = PassportValidation()
+                }
+                else -> {
+                    validateLine(line, passportValidator)
+                    if (it == input.inputData.lastIndex && passportValidator.isValid()) {
+                        numValidPassports++
+                    }
+                }
+            }
+        }
+        return numValidPassports
+    }
+
+    private fun validateLine(line: String, passportValidator: PassportValidation) {
+        line.split(" ")
+            .map {
+                val splitComponent = it.split(":")
+                Pair(splitComponent[0], splitComponent[1])
+            }
+            .map {
+                validate(it, passportValidator)
+            }
+    }
+
+    private fun validate(fieldValueMapping: Pair<String, String>, passportValidation: PassportValidation) {
+        when (fieldValueMapping.first) {
+            "byr" -> passportValidation.byr = true
+            "iyr" -> passportValidation.iyr = true
+            "eyr" -> passportValidation.eyr = true
+            "hgt" -> passportValidation.hgt = true
+            "hcl" -> passportValidation.hcl = true
+            "ecl" -> passportValidation.ecl = true
+            "pid" -> passportValidation.pid = true
+            "cid" -> passportValidation.cid = true
+            else -> return
+        }
     }
 
     override fun run() {
@@ -16,3 +60,19 @@ class Day4Pt1(
         println("Day 4 (pt.1): ${countValidPassports(input)}")
     }
 }
+
+data class PassportValidation(
+    var byr: Boolean = false,
+    var iyr: Boolean = false,
+    var eyr: Boolean = false,
+    var hgt: Boolean = false,
+    var hcl: Boolean = false,
+    var ecl: Boolean = false,
+    var pid: Boolean = false,
+    var cid: Boolean = true //Optional for now
+) {
+    fun isValid(): Boolean {
+        return cid && pid && ecl && hcl && hgt && eyr && iyr && byr
+    }
+}
+
